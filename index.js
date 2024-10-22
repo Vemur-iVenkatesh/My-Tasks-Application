1c34efd90c99fa3630ef7a136a5706deac784d8f
Vemur-iVenkatesh <vemuriivenkatesh@gmail.com>
2024-10-22 18:15:26 +0000

    intro to Js Part

---
+++ index.js
@@ -0,0 +1,8 @@
+const state={
+    tasklist:[],
+};
+const taskcontents=Document.querySelector("task_content");
+const taskmodal=Document.querySelector("task_modal_body");
+const htmltaskcontent=(id,title,Deadline,TaskFlow,url)=>{
+
+};
